b8dc5ddd09a1ad9bb8d9e3709c99da5b9c9bbed6
Icetail <51836525+Icetail1@users.noreply.github.com>
2023-04-04 21:41:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ window.onload = function () {
 }
 
 Array.prototype.remove = function(val) {
-  var index = this.indexOf(val)
+  var index = this.indexOf(val);
   if (index > -1) {
-    this.splice(index, 1)
+    this.splice(index, 1);
   }
 }
 /**
