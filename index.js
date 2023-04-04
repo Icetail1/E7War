77e0e9482dbd4be203f00921bea01cd235500bc5
Icetail <51836525+Icetail1@users.noreply.github.com>
2023-04-04 21:41:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ window.onload = function () {
     mainInterface();
 }
 
+Array.prototype.remove = function(val) {
+  var index = this.indexOf(val)
+  if (index > -1) {
+    this.splice(index, 1)
+  }
+}
 /**
  * 主界面里的逻辑
  */
