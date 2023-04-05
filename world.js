16a4027f3dfadf002d815e4e0f153b0ad30c61ec
Icetail <51836525+Icetail1@users.noreply.github.com>
2023-04-05 20:46:13 +0900

    Update world.js

--- world.js
+++ world.js
@@ -273,13 +273,13 @@ class World {
         ctx.font = "16px Microsoft YaHei";
         ctx.fillStyle = "black";
         ctx.textAlign = "left";
-        ctx.fillText("当前本数：" + this.mainLevel, 20, 60);
-        ctx.fillText("金钱：" + this.user.money.toString(), 20, 20);
-        ctx.fillText("怪物数量：" + this.monsters.size, 20, 40);
-        ctx.fillText("炮塔数量：" + this.batterys.length, 20, 60);
-        ctx.fillText("炮塔数量：" + this.batterys.length, 20, 60);
-        ctx.fillText("下一波：" + this.monsterFlow.toString(), 20, 80);
-        ctx.fillText("当前波数：" + (this.monsterFlow.level - 1), 20, 100);
-        ctx.fillText("倒计时：" + (this.monsterFlow.delayTick), 20, 120);
+        ctx.fillText("当前本数：" + this.mainLevel, 20, 20);
+        ctx.fillText("金钱：" + this.user.money.toString(), 20, 40);
+        ctx.fillText("怪物数量：" + this.monsters.size, 20, 60);
+        ctx.fillText("炮塔数量：" + this.batterys.length, 20, 80);
+        ctx.fillText("炮塔数量：" + this.batterys.length, 20, 80);
+        ctx.fillText("下一波：" + this.monsterFlow.toString(), 20, 100);
+        ctx.fillText("当前波数：" + (this.monsterFlow.level - 1), 20, 120);
+        ctx.fillText("倒计时：" + (this.monsterFlow.delayTick), 20, 140);
     }
 }
