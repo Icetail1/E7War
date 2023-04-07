575e912b1eb85ace5515dfb017fa0570a4bc13fc
Icetail <51836525+Icetail1@users.noreply.github.com>
2023-04-07 15:41:09 +0900

    Update monster.js

--- monster/monster.js
+++ monster/monster.js
@@ -30,7 +30,7 @@ class Monster extends CircleObject {
         this.hpInit(10);
         this.colishDamage = 10;
         this.r = 5;  // 身体半径
-        this.addPrice = 5;  // 奖金
+        this.addPrice = 1;  // 奖金
         this.destination = new Vector(this.world.width / 2, this.world.height / 2);  // 默认的目的地是中心点
         this.bodyColor = MyColor.arrTo([25, 25, 25, 0.8]);
         this.hpColor = MyColor.arrTo([200, 20, 20, 0.5]);
