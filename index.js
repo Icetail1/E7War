4b3562a3fa0fb453c8bb94d09e0ae7a12c5a870e
Icetail <51836525+Icetail1@users.noreply.github.com>
2023-04-07 15:18:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function levelUp(val){
      val.level += 1;
      val.hp    *= 2;    
      val.maxHp *= 2;
-     val.damage += 1;
+     val.damage *= 2;
      val.r *= 2;
   }else
   {
      val.level += 1;
      val.hp    *= 3;  
      val.maxHp *= 3;
-     val.damage += 2;
+     val.damage *= 2;
      val.r *= 2;
   }
 }
