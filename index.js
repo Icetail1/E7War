eaf3cc227bf9252fd91f21aeee158679ed53c2d7
Icetail <51836525+Icetail1@users.noreply.github.com>
2023-04-10 08:51:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -897,20 +897,20 @@ let refreshBoxPanel = setInterval(() => {
              world.user.money += 1;
 
          } 
-     }, 1000);   
+     }, 5000);   
     
 let archerForfour = setInterval(() => {
          if (world.fourArcherBuff && !isGamePause) {
              world.user.money += 1;
 
          } 
-     }, 1000);   
+     }, 5000);   
     
 let archerForSix = setInterval(() => {
          if (world.sixArcherBuff && !isGamePause) {
              world.user.money += 3;
 
          } 
-     }, 1000);   
+     }, 5000);   
 
 }
